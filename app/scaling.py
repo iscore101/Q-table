7d287c890e91a0b3d03adbebba3a9c8affc96019
peter-popescu <peter.popescuu@gmail.com>
2024-01-21 19:26:44 -0500

    debug and minor refactor scaling.py

--- app/scaling.py
+++ app/scaling.py
@@ -4,7 +4,6 @@ from modified_ds2 import DirectedGraph
 from flask import Flask, request
 import numpy as np
 import json
-import time
 import base64
 
 app = Flask(__name__)
@@ -13,42 +12,40 @@ app = Flask(__name__)
 @app.route('/metrics', methods=['POST']) 
 #def update_json_data():
 def receive_metrics():
-    global json_data, graph
+    global json_data, graph, _q_learner
     print("===> receive_metrics is called")
-    data = request.data.decode('utf-8')
-    json_data = data
-    print(f'===> json_data: {json_data}')
 
-    if json_data is not None:
-        print("===> before graph in main")
+    if json_data is None:
+        print("===> first-time init")
+        json_data = request.data.decode('utf-8')
         graph = build_graph(json_data)
-        q_learner_instance = q_learner(len(graph.vertices), 3, 3, 3, 3, 3, 3, 0.5, 0.5, 0, graph)
-        q_learner_instance.populate_q_table_offline()
+        _q_learner = q_learner(len(graph.vertices), 3, 3, 3, 3, 3, 3, 0.5, 0.5, 0, graph)
+        _q_learner.populate_q_table_offline()
+    else:
+        json_data = request.data.decode('utf-8') # update global var with metrics
+
+    print(f'===> json_data: {json_data}')
     return f'json_data: {json_data}'
 
 # @app.route('/', methods=['GET'])
 @app.route('/parallelisms', methods=['GET'])
 #def send_action():
 def get_parallelism():
-    global action
-    previous_json_data = None
+    global json_data, last_json_data, graph, _q_learner
     print("===> get_parallelism is called")
-    if json_data != previous_json_data:
+
+    if json_data != last_json_data:
         print("===> Will compute new action")
         state = get_latest_metrics(json_data, graph.sorted_vertices)
-        actions = q_learner.online_generate_action(state)
-        action_dict = {graph.sorted_vertices[i]: a for i, a in enumerate(actions)}
-        action = action_dict # update global variable for get requesst
-        reward = get_reward(graph.sink.name)
-        q_learner.online_update_q_table(reward)
+        actions = _q_learner.online_generate_action(state)
+        action = {graph.sorted_vertices[i]: a for i, a in enumerate(actions)}
+        reward = get_reward(json_data, graph.sink.name)
+        _q_learner.online_update_q_table(reward)
 
-        previous_json_data = json_data # Reset previous_json_data after processing 
+        last_json_data = json_data # set last_json_data after processing
 
-    if action is not None:
         print(f'action: {action}')
         return f'action: {action}'
-    else:
-        return 'No action.'
 
 def get_reward(json_data, sink_id):
     try:
@@ -119,9 +116,8 @@ def get_latest_metrics(json_data, index_to_id_dict):
     except Exception as e:
         print(f'Error: {e}')
 
-def decode_vertex_bytes(vertex):
+def decode_vertex_bytes(vertex_bytes):
     ''' Decode the vertex ID from base64 to hexadecimal. '''
-    vertex_bytes = vertex['bytes']
     decoded_bytes = base64.b64decode(vertex_bytes)
     hex_result = decoded_bytes.hex()
     return hex_result
@@ -136,22 +132,22 @@ def build_graph(json_data):
         data = json.loads(json_data)
 
         topology = data['jobTopology']
-        graph = DirectedGraph(0)
-        for vertex in topology['verticesInTopologicalOrder']:
-            vertex_id = decode_vertex_bytes(vertex['bytes'])
-            graph.add_vertex(Vertex(vertex_id, 0, 0))
-        sorted_vertices = graph.topological_sort()
-        graph.set_source(graph.vertices[sorted_vertices[0]])
-        graph.set_sink(graph.vertices[sorted_vertices[-1]])
+        new_graph = DirectedGraph(0)
+        for vertex_dict in topology['verticesInTopologicalOrder']:
+            vertex_id = decode_vertex_bytes(vertex_dict['bytes'])
+            new_graph.add_vertex(Vertex(vertex_id, 0, 0))
+        sorted_vertices = new_graph.topological_sort()
+        new_graph.set_source(new_graph.vertices[sorted_vertices[0]])
+        new_graph.set_sink(new_graph.vertices[sorted_vertices[-1]])
 
         for from_vertex_name, to_vertices in topology['outputs'].items():
-            from_vertex = graph.vertices[from_vertex_name]
-            for to_vertex in to_vertices:
-                to_vertex_bytes = to_vertex['bytes']
-                to_vertex = graph.vertices[decode_vertex_bytes(to_vertex_bytes)]
-                graph.add_edge(from_vertex, to_vertex)
+            from_vertex = new_graph.vertices[from_vertex_name]
+            for to_vertex_dict in to_vertices:
+                to_vertex_bytes = to_vertex_dict['bytes']
+                to_vertex = new_graph.vertices[decode_vertex_bytes(to_vertex_bytes)]
+                new_graph.add_edge(from_vertex, to_vertex)
         
-        return graph
+        return new_graph
 
     except Exception as e:
         print(f'Error: {e}')
@@ -159,7 +155,9 @@ def build_graph(json_data):
 if __name__ == '__main__':
     print("===> starting main")
     json_data = None
-    action = None
+    last_json_data = None
+    graph = None
+    _q_learner = None
     app.run(host='0.0.0.0', port=5001)
 
     # while json_data is None:
@@ -170,9 +168,9 @@ if __name__ == '__main__':
     # q_learner = q_learner(len(graph.vertices), 3, 3, 3, 3, 3, 3, 0.5, 0.5, 0, graph)
     # q_learner.populate_q_table_offline()
 
-    # previous_json_data = None
+    # last_json_data = None
     # # while True:
-    # #     if json_data != previous_json_data:
+    # #     if json_data != last_json_data:
     # #         print("===> Will compute new action")
     # #         state = get_latest_metrics(json_data, graph.sorted_vertices)
     # #         actions = q_learner.online_generate_action(state)
@@ -181,10 +179,10 @@ if __name__ == '__main__':
     # #         reward = get_reward(graph.sink.name)
     # #         q_learner.online_update_q_table(reward)
 
-    # #         previous_json_data = json_data # Reset previous_json_data after processing
+    # #         last_json_data = json_data # Reset last_json_data after processing
 
     # #     time.sleep(0.25) 
-    # if json_data != previous_json_data:
+    # if json_data != last_json_data:
     #     print("===> Will compute new action")
     #     state = get_latest_metrics(json_data, graph.sorted_vertices)
     #     actions = q_learner.online_generate_action(state)
@@ -193,4 +191,4 @@ if __name__ == '__main__':
     #     reward = get_reward(graph.sink.name)
     #     q_learner.online_update_q_table(reward)
 
-    #     previous_json_data = json_data # Reset previous_json_data after processing 
+    #     last_json_data = json_data # Reset last_json_data after processing 
